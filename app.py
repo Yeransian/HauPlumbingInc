6ca72be5914b5be1820691b2498252861f2ad7d3
AbrahamYeransian <abraham@innoviuz.com>
2021-06-27 18:01:13 -0500

    Author: Abraham Yeransian Description: I changed some routes, I set up an API route to accept info requests from the front end.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import os
-
+from os import path
+from api import api_routes
 
 TEMPLATE_DIR = os.path.abspath('../templates')
 STATIC_DIR = os.path.abspath('../static')
+global_base_dir = path.join(path.dirname(path.realpath(__file__)))
+db_dir = path.join(global_base_dir, 'db')
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -14,8 +16,23 @@ def hello_world():
 
 @app.route('/about')
 def test_temp():
-    return render_template('home.html')
+    return render_template('about.html')
 
+
+@app.route('/invoice')
+def invoice():
+    print('Opened invoice')
+    return render_template('invoice.html')
+
+# API routes are below
+
+@app.route('/api/submit_info_request', methods=['POST'])
+def submit_info_request():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    phone = request.args.get('phone')
+    print(name, email, phone)
+    return {'name': name, 'email': email, 'phone': phone}
 
 if __name__ == '__main__':
     app.run()
---
+++ static/assets/js/submit_inquiry.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#submit_inquiry').click(function () {
+        console.log('I clicked the button!!')
+        $.ajax({
+
+        })
+    });
+
+
+});
